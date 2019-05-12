1e7372012772a7bdecd1a6d50c8c6e43ede472f0
Martin Thomas <mathomas@e1z2r9p28.42.us.org>
2019-05-12 12:42:23 -0700

    fixed: ft_memalloc.c

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: mathomas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 15:46:42 by mathomas          #+#    #+#             */
-/*   Updated: 2019/05/12 12:26:25 by mathomas         ###   ########.fr       */
+/*   Updated: 2019/05/12 12:41:53 by mathomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	*ft_memalloc(size_t size)
 	if (mem == NULL)
 		return (NULL);
 	while (++i < size)
-		mem[i] = 0;
+		mem[i] = '\0';
 	return (mem);
 }
